2d6c09a99289c74793ffa2b1513b1eaf9e427ee3
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-04-14 23:35:38 -0400

    done with the development side of this project

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Task Main Module
 
 # import necessary modules
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from rbm import RestrictedBoltzmannMachine
@@ -29,10 +30,10 @@ def main():
     # train the restricted boltzmann machine with varying learning rates
     learning_rates = [0.001, 0.01, 0.1]
     num_epochs = range(150)
-    for learning_rate in learning_rates:
+    abs_mean_errors = np.zeros((len(learning_rates), len(num_epochs)))
+    for i, learning_rate in enumerate(learning_rates):
 
-        abs_mean_errors = []
-        for epochs in num_epochs:
+        for j, epochs in enumerate(num_epochs):
 
             # initialize restricted boltzmann machine
             rbm = RestrictedBoltzmannMachine(num_visible_units=10, num_hidden_units=num_hidden_units)
@@ -47,16 +48,19 @@ def main():
             abs_mean_error = rbm.absolute_mean_error(ice_cream, predictions)
 
             # append the error to the list
-            abs_mean_errors.append(abs_mean_error)
+            abs_mean_errors[i, j] = abs_mean_error
 
-        # plot the absolute mean error results
-        plt.plot(num_epochs, abs_mean_errors, label=f'learning rate = { learning_rate }')
-        plt.title('Absolute Mean Error of Predicted Labels After Training')
-        plt.xlabel('number of epochs')
-        plt.ylabel('absolute mean error')
-        plt.legend()
-        plt.text(75, 3.75, f'number of hidden units = { num_hidden_units }')
-        plt.show()
+    # plot the absolute mean error results
+    plt.plot(num_epochs, abs_mean_errors[0, :], color='red', label=f'learning rate = { learning_rates[0] }')
+    plt.plot(num_epochs, abs_mean_errors[1, :], color='green', label=f'learning rate = { learning_rates[1] }')
+    plt.plot(num_epochs, abs_mean_errors[2, :], color='blue',  label=f'learning rate = { learning_rates[2] }')
+    plt.title('Absolute Mean Error of Predicted Labels After Training')
+    plt.xlabel('number of epochs')
+    plt.ylabel('absolute mean error')
+    plt.legend()
+    plt.text(90, 4, f'number of hidden units = { num_hidden_units }')
+    plt.text(90, 3.8, f'search and converge constant = { converge_constant }')
+    plt.show()
 
 
 if __name__ == '__main__':
